2279c26699ed306c7753bb435312d0b1b85d81c5
Michael <mjmjulien10@gmail.com>
2018-09-11 08:24:13 -0600

    added data members (private)

--- src/model/MarshmallowMonster.java
+++ src/model/MarshmallowMonster.java
@@ -2,6 +2,13 @@ package model;
 
 public class MarshmallowMonster 
 {
+	//Data member section
+	//They are ALWAYS private
+	private String name;
+	private double legCount;
+	private int eyeCount;
+	private boolean hasNoses;
+	private int armCount;
 	public MarshmallowMonster() 
 	{
 
