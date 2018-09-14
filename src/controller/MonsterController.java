12a409fb1f913fbf526c474f098fa15dd3d9fe74
Michael <mjmjulien10@gmail.com>
2018-09-14 09:06:29 -0600

    added more JOptionPane

--- src/controller/MonsterController.java
+++ src/controller/MonsterController.java
@@ -24,12 +24,52 @@ public class MonsterController
 		System.out.println(myMonster);
 		JOptionPane.showMessageDialog(null, myMonster);
 		myMonster.setArmCount(25);
-		System.out.println("My monster has this many arms: " + myMonster.getArmCount());
+		//System.out.println("My monster has this many arms: " + myMonster.getArmCount());
 		JOptionPane.showMessageDialog(null, "My monster has this many arms: " + myMonster.getArmCount());
 		
-		JOptionPane.showInputDialog(null, "What is your monster's name?");
+		
 		
 		//Make a new monster and customize from user input!
-		MarshmallowMonster userMonster;
+		MarshmallowMonster userMonster = new MarshmallowMonster();
+		String userMonsterName = JOptionPane.showInputDialog(null, "What is your monster's name?");
+		String userNumber = JOptionPane.showInputDialog(null, "How many arms?");
+		if (validInt(userNumber))
+		{
+			int arms = Integer.parseInt(userNumber);
+			userMonster.setArmCount(arms);
+		}
+		
+	}
+	
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "Please type an integer value goofy head");
+		}
+		return isValid;
+	}
+	
+	public boolean validDouble(String maybeDouble)
+	{
+		boolean isValid = false;
+		
+		try
+		{
+			Double.parseDouble(maybeDouble);
+			isValid = true;
+		}
+		catch(NumberFormatException error)
+		{
+			JOptionPane.showMessageDialog(null, "Please type a double value monky");
+		}
+		return isValid;
 	}
 }
